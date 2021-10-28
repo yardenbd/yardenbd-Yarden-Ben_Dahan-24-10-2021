f7ec3a76ee29c8c8f023f5cbc22f8a2b859888d8
yardenbd <yarden9826@gmail.com>
2021-10-28 16:14:57 +0300

    fix

---
+++ src/url.js
@@ -0,0 +1,11 @@
+export const apiKey = "NSuxTf0j94EkQApMxQSc39vXSy814qj2";
+export const forecastURL ='https://dataservice.accuweather.com/currentconditions/v1/'
+export const serverURL =
+  "https://dataservice.accuweather.com/locations/v1/cities/autocomplete";
+export const defaultNextDaysURL='https://dataservice.accuweather.com/forecasts/v1/daily/5day/'
+ export  const currentForecastURL =
+  "https://dataservice.accuweather.com/currentconditions/v1/";
+export const defaultCityURL =
+  "https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=NSuxTf0j94EkQApMxQSc39vXSy814qj2&q=tel%20aviv";
+  export const getNextDaysForecast ="https://dataservice.accuweather.com/forecasts/v1/daily/5day/"
+  export const geolocationURL = 'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search'
